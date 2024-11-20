cd66c201cadfcd9ea1d76ebcbccce7eed8208ba2
Cuflic Dan <84440467+Dany20y@users.noreply.github.com>
2024-11-21 01:29:31 +0200

    wet

---
+++ WebApp.Domain/Entities/DatabaseTables/CoCardDBTable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Domain.Entities.DatabaseTables
+{
+    public class CoCardDBTable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id;
+
+        [Required]
+        [Display(Name = "Title")]
+        public string title;
+
+        [Display(Name = "Description")]
+        public string description;
+
+        [Display(Name = "Image")]
+        public byte[] img;
+
+        [Display(Name = "Pdf File")]
+        public byte[] pdf_file;
+    }
+}
